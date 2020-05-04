5d29bf6936d1da0de1dd73afdad3844b0df0d098
zhangweitao <zhangweitao@chinaredstar.cn>
2020-05-04 21:02:20 +0800

    'mcmc'

--- 第一章-统计学习及监督学习概论/code.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-"""
-@File    : code.py
-@Time    : 2019/12/10 18:29
-@Author  : zwt
-@git   : 
-@Software: PyCharm
-"""
---
+++ 采样/metropolis_hastings.py
@@ -0,0 +1,43 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# author: zwt
+# datetime: 2020/5/4 17:51
+
+import numpy as np
+import scipy.stats as st
+import seaborn as sns
+
+
+mus = np.array([5, 5])
+sigmas = np.array([[1, .9], [.9, 1]])
+
+
+def circle(x, y):
+    return (x-1)**2 + (y-2)**2 - 3**2
+
+
+def pgauss(x, y):
+    return st.multivariate_normal.pdf([x, y], mean=mus, cov=sigmas)
+
+
+def metropolis_hastings(p, iter=1000):
+    x, y = 0., 0.
+    samples = np.zeros((iter, 2))
+
+    for i in range(iter):
+        x_star, y_star = np.array([x, y]) + np.random.normal(size=2)
+        if np.random.rand() < p(x_star, y_star) / p(x, y):
+            x, y = x_star, y_star
+        samples[i] = np.array([x, y])
+
+    return samples
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    samples = metropolis_hastings(circle, iter=100)
+    sns.jointplot(samples[:, 0], samples[:, 1])
+
+    samples = metropolis_hastings(pgauss, iter=100)
+    sns.jointplot(samples[:, 0], samples[:, 1])
+    plt.show()
